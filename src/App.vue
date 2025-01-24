e5a549dfd402b77b58f2dd8351da99a17a1c109c
Christian Silva <tininjacms@gmail.com>
2025-01-24 13:19:14 -0300

    feat(background): Add o a imagem ao fundo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <router-view/>
+  <div class="h-screen bg-img bg-no-repeat bg-center bg-[url(@/assets/images/background-desktop.png)]">
+    <router-view />
+  </div>
 </template>
 
 <style>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+.bg-img {
+  background-size: 100% 100%;
+}
 </style>
