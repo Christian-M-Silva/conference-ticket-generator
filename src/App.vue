523ba70607ccb01a30fb8bfbdcb74a50ccff498f
Christian Silva <tininjacms@gmail.com>
2025-01-24 17:06:50 -0300

    feat(layoutHome): Add estilização e construção inicial da tela home sem o formulario

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="static h-screen bg-img bg-no-repeat bg-center bg-[url(@/assets/images/background-desktop.png)]">
+  <div
+    class="static h-screen bg-img bg-no-repeat bg-center bg-[url(@/assets/images/background-desktop.png)]"
+  >
+    <img
+      class="absolute top-0 right-0"
+      src="@/assets/images/pattern-squiggly-line-top.svg"
+      alt="Detalhe superior"
+    />
+    <img
+      class="absolute bottom-0 left-0"
+      src="@/assets/images/pattern-squiggly-line-bottom.svg"
+      alt="Detalhe inferior"
+    />
     <router-view />
-    <img class="absolute top-0 right-0" src="@/assets/images/pattern-squiggly-line-top.svg" alt="">
-    <img class="absolute bottom-0 left-0" src="@/assets/images/pattern-squiggly-line-bottom.svg" alt="">
   </div>
 </template>
 
@@ -10,6 +20,19 @@
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+@font-face {
+  font-family: "Inconsolata";
+  font-style: normal;
+  font-weight: 200 700;
+  font-display: swap;
+  src: url("@/assets/fonts/Inconsolata-VariableFont_wdth,wght.ttf")
+    format("ttf");
+}
+
+* {
+  font-family: "Inconsolata", monospace;
+}
 
 .bg-img {
   background-size: 100% 100%;
--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Olá</div>
+  <div class="text-white flex flex-col items-center">
+    <img class="mt-12 mb-14" src="@/assets/images/logo-full.svg" alt="Logo" />
+    <div class="text-5xl font-bold text-center space-words mb-4">
+      Your Journey to Coding Conf <br />
+      <div class="mt-3">2025 Starts Here!</div>
+    </div>
+    <span class="text-[1.4rem] text-slate-300"
+      >Secure your spot at next year´s biggest coding conference</span
+    >
+    <form class="mt-11" action="">Formulario</form>
+  </div>
 </template>
 <script src="./home.ts"></script>
 <style scoped src="./home.css"></style>
